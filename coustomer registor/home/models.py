dc4a2f3ff228d4a2ece001013e4b2fbe0aedde84
sundarraj2003 <142962207+sundarraj2003@users.noreply.github.com>
2024-02-28 10:15:14 +0530

    customer register in django

---
+++ coustomer registor/home/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Datas(models.Model):
+    Name=models.CharField(max_length=20,default="")
+    Age=models.IntegerField(default="")
+    Address=models.CharField(max_length=50,default="")
+    Contact=models.CharField(max_length=20,default="")
+    Mail=models.CharField(max_length=50,default="")
